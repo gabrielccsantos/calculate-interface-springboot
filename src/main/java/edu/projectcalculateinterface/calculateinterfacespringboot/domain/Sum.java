c53d9a09776460fa13ef023b6ad11c122e5a8cd8
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-06-26 07:34:13 -0300

    Classe SUm

---
+++ src/main/java/edu/projectcalculateinterface/calculateinterfacespringboot/domain/Sum.java
@@ -0,0 +1,9 @@
+package edu.projectcalculateinterface.calculateinterface.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Sum extends edu.projectcalculateinterface.calculateinterface.domain.Operation {
+
+    public Sum(){}
+}
