95698b5bf39e934b112f771a17ce317b8296b346
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-06-26 07:35:27 -0300

    Classe Multi

---
+++ src/main/java/edu/projectcalculateinterface/calculateinterfacespringboot/domain/Multi.java
@@ -0,0 +1,9 @@
+package edu.projectcalculateinterface.calculateinterface.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Multi extends edu.projectcalculateinterface.calculateinterface.domain.Operation {
+    public Multi(){}
+
+}
