9b43e81c792a2910cd0e308cff918191ae95c5b5
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-06-26 07:34:42 -0300

    Classe sub

---
+++ src/main/java/edu/projectcalculateinterface/calculateinterfacespringboot/domain/Sub.java
@@ -0,0 +1,9 @@
+package edu.projectcalculateinterface.calculateinterface.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Sub extends edu.projectcalculateinterface.calculateinterface.domain.Operation {
+    public Sub(){
+    }
+}
