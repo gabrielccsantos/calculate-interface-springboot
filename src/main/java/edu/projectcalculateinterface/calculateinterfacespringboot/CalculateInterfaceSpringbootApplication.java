69efe518952baf0ad6f25c6197232d20503eeeab
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-06-26 07:32:35 -0300

    Criando repo e configurando H2

---
+++ src/main/java/edu/projectcalculateinterface/calculateinterfacespringboot/CalculateInterfaceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package edu.projectcalculateinterface.calculateinterfacespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateInterfaceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculateInterfaceSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/projectcalculateinterface/calculateinterfacespringboot/CalculateInterfaceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.projectcalculateinterface.calculateinterfacespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateInterfaceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
