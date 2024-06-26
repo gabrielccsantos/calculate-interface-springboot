1e8b86afbf2d235fd4e4b5c8cab31a73966e8b06
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-06-26 07:36:02 -0300

    Classe Division

---
+++ src/main/java/edu/projectcalculateinterface/calculateinterfacespringboot/domain/Division.java
@@ -0,0 +1,8 @@
+package edu.projectcalculateinterface.calculateinterface.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Division extends edu.projectcalculateinterface.calculateinterface.domain.Operation {
+    public Division(){}
+}
